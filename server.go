c8476044cec46354ef44001d649cd94bb6477864
Bochen Xu <bxu10@illinois.edu>
2019-03-08 18:19:22 -0500

    interruptted by a pull

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"strconv"
@@ -33,9 +34,25 @@ func intHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+type mockHandler struct {
+}
+
+func (m *mockHandler) ServeHttp(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "count is %d\n", h.n)
+}
+
 func main() {
 	go intHandlerHelper()
+
+	mux := http.NewServeMux()
+	// mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(staticPath))))
+	ts := NewTasksServer()
+	mux.Handle("/task.js", ts)
+	mux.Handle("/submit", nil)
+	mux.Handle("/stats/", nil)
+
 	http.HandleFunc("/", intHandler)
 	http.ListenAndServeTLS(":8888", "server.crt",
-		"server.key", nil)
+		"server.key", mux)
+
 }
